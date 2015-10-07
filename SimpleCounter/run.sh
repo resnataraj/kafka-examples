319a7396f29d120d6cd0320a15441d7dc602c0d9
resnataraj <nataraj.jonnalagadda@gmail.com>
2015-10-07 12:39:20 -0700

    Update run.sh

--- SimpleCounter/run.sh
+++ SimpleCounter/run.sh
@@ -1,1 +1,1 @@
-java -cp target/uber-SimpleCounter-1.0-SNAPSHOT.jar com.shapira.examples.producer.simplecounter.SimpleCounter localhost:9092 video1 new async 500 10
+java -cp target/uber-SimpleCounter-1.0-SNAPSHOT.jar com.shapira.examples.producer.simplecounter.SimpleCounter sfdvgctmn001.gid.gap.com:9091 nat_topic new async 500 10
